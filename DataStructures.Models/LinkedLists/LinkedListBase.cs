2a7ac5cde3f1a08288fc7150578f26161f9632de
neckrall <neckrall@proton.me>
2026-01-11 22:09:23 +0300

    Add base abstraction for linked lists
    Introduce an abstract linked list base class and item contract
    to support multiple linked list implementations and shared tests.
    Focus on core operations and Big O characteristics.

---
+++ DataStructures.Models/LinkedLists/LinkedListBase.cs
@@ -0,0 +1,27 @@
+﻿namespace DataStructures.Models.LinkedLists;
+
+public abstract class LinkedListBase<TItem, TValue> where TItem : LinkedListItemBase<TValue>
+{
+    // State
+    public TItem? First { get; protected set; }
+    public TItem? Last { get; protected set; }
+    public int Count { get; protected set; }
+
+    // Contracts
+    public abstract void Add(TValue value);
+    public abstract void AddToBegin(TValue value);
+    public abstract TItem AddAfter(TItem targetItem, TValue value);
+    public abstract TItem AddBefore(TItem targetItem, TValue value);
+    public abstract TItem? Find(TValue value);
+    public abstract void RemoveFirst();
+    public abstract void RemoveLast();
+    public abstract bool Remove(TValue value);
+    public abstract bool Remove(TItem item);
+}
+
+public abstract class LinkedListItemBase<T> 
+{
+    protected LinkedListItemBase(T value) => Value = value;
+
+    public T? Value { get; init; }
+}
