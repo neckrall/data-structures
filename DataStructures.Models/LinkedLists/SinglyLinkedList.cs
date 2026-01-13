ae9d6e311b8c19e21333f99fd4d5e1cea4711f0c
neckrall <neckrall@proton.me>
2026-01-13 15:54:01 +0300

    Initialize singly linked list implementation

---
+++ DataStructures.Models/LinkedLists/SinglyLinkedList.cs
@@ -0,0 +1,56 @@
+﻿namespace DataStructures.Models.LinkedLists;
+
+public sealed class SinglyLinkedList<T> : LinkedListBase<SinglyLinkedListItem<T>, T>
+{
+    public override void Add(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override SinglyLinkedListItem<T> AddAfter(SinglyLinkedListItem<T> targetItem, T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override SinglyLinkedListItem<T> AddBefore(SinglyLinkedListItem<T> targetItem, T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void AddToBegin(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override SinglyLinkedListItem<T>? Find(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Remove(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Remove(SinglyLinkedListItem<T> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool RemoveFirst()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool RemoveLast()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public sealed class SinglyLinkedListItem<T> : LinkedListItemBase<T>
+{
+    public SinglyLinkedListItem(T value) : base(value) { }
+
+    public SinglyLinkedListItem<T>? Next { get; set; }
+}
---
+++ DataStructures.Tests/SinglyLinkedListTests.cs
@@ -0,0 +1,8 @@
+﻿using DataStructures.Models.LinkedLists;
+
+namespace DataStructures.Tests;
+
+public sealed class SinglyLinkedListTests : LinkedListsTests<SinglyLinkedListItem<int>>
+{
+    protected override LinkedListBase<SinglyLinkedListItem<int>, int> CreateList() => new SinglyLinkedList<int>();
+}
