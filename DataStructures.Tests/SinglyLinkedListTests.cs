dac73b890e54b50b9f8a47e2c890efb62b5db140
neckrall <neckrall@proton.me>
2026-01-15 21:50:14 +0300

    Add implementation-specific tests for SinglyLinkedList AddToBegin

--- DataStructures.Tests/SinglyLinkedListTests.cs
+++ DataStructures.Tests/SinglyLinkedListTests.cs
@@ -40,4 +40,41 @@ public sealed class SinglyLinkedListTests : LinkedListsTests<SinglyLinkedListIte
         Assert.Null(list.Last!.Next);
     }
     #endregion
+
+    #region AddToBegin
+    [Fact]
+    public void AddToBegin_WhenListIsEmpty_ShouldSetNextToNull()
+    {
+        // Arrange
+        var list = CreateList();
+        var value = 8;
+
+        // Act
+        list.AddToBegin(value);
+
+        // Assert
+        Assert.Null(list.First!.Next);
+        Assert.Null(list.Last!.Next);
+    }
+
+    [Fact]
+    public void AddToBegin_WhenListIsNotEmpty_ShouldPrependItemAndLinkNodesCorrectly()
+    {
+        // Arrange
+        var list = CreateList();
+        var oldFirstValue = 1;
+        var newFirstValue = 2;
+
+        list.Add(oldFirstValue);
+
+        var oldFirstItem = list.First;
+
+        // Act
+        list.AddToBegin(newFirstValue);
+
+        // Assert
+        Assert.Same(list.First!.Next, oldFirstItem);
+        Assert.Null(list.Last!.Next);
+    }
+    #endregion
 }
